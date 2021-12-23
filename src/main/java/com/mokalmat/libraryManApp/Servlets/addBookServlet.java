2da7b691e27ca2417cae17729372e264ec1459dc
HebaObaia <heba.obaia@mokalmat.com>
2021-12-23 21:41:18 +0200

    inputstream

--- src/main/java/com/mokalmat/libraryManApp/Servlets/addBookServlet.java
+++ src/main/java/com/mokalmat/libraryManApp/Servlets/addBookServlet.java
@@ -1,5 +1,7 @@
 package com.mokalmat.libraryManApp.Servlets;
 
+import java.beans.Statement;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,11 +15,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 import libraryManApp.Books;
 import libraryManApp.Response;
-import libraryManApp.getConnectionLibrary;
+
 
 /**
  * Servlet implementation class addBookServlet
@@ -29,6 +29,8 @@ public class addBookServlet extends HttpServlet {
     /**
      * @see HttpServlet#HttpServlet()
      */
+	
+		
     public addBookServlet() {
         super();
         // TODO Auto-generated constructor stub
@@ -47,40 +49,80 @@ public class addBookServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	
+	  
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String DB_URL="jdbc:postgresql://localhost:5432/Library";
-		String userName="postgres";
-		String password="database";
 		Gson gson = new Gson();
 		Response resSucc =new Response();
 	    Response resFa =new Response();
-	    Response hamada = new Response();
-	    Response ca =new Response();
 		resSucc.setStatusCode(0);
 		resFa.setStatusCode(-1);
-		hamada.setStatusCode(-2);
-		ca.setStatusCode(-3);
 		String jsonStrSucc = gson.toJson(resSucc);
 		String jsonStrFa= gson.toJson(resFa);
-		String jsonHamada = gson.toJson(hamada);
-		String jsonCa = gson.toJson(ca);
 		PrintWriter writer = response.getWriter();
 
 		try {
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.setHeader("Access-Control-Allow-Methods", "POST");
+			Class.forName("org.postgresql.Driver");
+			String DB_URL="jdbc:postgresql://localhost:5432/Library";
+			String userName="postgres";
+			String password="database";
+			String queryInsert, queryCreate;
+			PreparedStatement pstCreate, pstInsert;
 			Connection connection = DriverManager.getConnection(DB_URL, userName, password);
-			//Connection connection = DriverManager.getConnection(DB_URL, userName, password);
-			System.out.println("Success");
-			
 			if(connection==null) {
-				writer.print(jsonStrFa); //(-1)
+				System.out.println("Not");
 			}
 			else {
-			writer.print(jsonStrSucc);}  //(0)
+			System.out.println("Opened");
+			}  
+			InputStream in = request.getInputStream();
+		    InputStreamReader streamReader = new InputStreamReader(in);
+		    Books book = gson.fromJson(streamReader, Books.class);
+		    
+			/*String bookID=request.getParameter("Id");
+			int IntID = Integer.parseInt(bookID);
+			String title=request.getParameter("title");
+			String publisher = request.getParameter("publisher");
+			String category =request.getParameter("category");
+			String price = request.getParameter("price");
+			
+			Books book =new Books();
+			book.setID(IntID);
+			book.setTitle(title);
+			book.setPublisher(publisher);
+			book.setCategory(category);
+			book.setPrice(price);*/
+			System.out.println("Added in book class");
+			System.out.println(
+					"ID" +book.getID()
+					+"title"+book.getTitle()
+					);
+			
+			/*queryCreate="create table bookInfo (id int, title varchar (200), publisher varchar (150), category varchar (100), price varchar (50), primary key(id))";
+			pstCreate=connection.prepareStatement(queryCreate);
+			pstCreate.execute();
+			writer.print(jsonStrSucc);
+            System.out.println("table Added");*/
+			
+			queryInsert="insert into bookInfo (id, title, publisher, category, price) values (?, ?, ?, ?, ?)";
+			pstInsert=connection.prepareStatement(queryInsert);
+			
+			pstInsert.setInt(1, book.getID());
+			pstInsert.setString(2, book.getTitle());
+			pstInsert.setString(3, book.getPublisher());
+			pstInsert.setString(4,book.getCategory());
+			pstInsert.setString(5, book.getPrice());
+			pstInsert.executeUpdate();
+            //connection.commit();
+            writer.print(jsonStrSucc);
+            System.out.println("Inserted");
+			
+			
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
-        	writer.print(jsonCa);   //(-3)
 		}
-		writer.print(jsonHamada); //(-2)
 	}
 
 }

80c03684f1cf7868267339dc861adc2f6a4a36ef
heba zain <heba.obaia@mokalmat.com>
2022-01-04 13:52:07 +0200

    loggers logback

--- src/main/java/com/mokalmat/libraryManApp/Servlets/UserBooksAction.java
+++ src/main/java/com/mokalmat/libraryManApp/Servlets/UserBooksAction.java
@@ -16,6 +16,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//import org.apache.taglibs.standard.lang.jstl.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -29,8 +33,8 @@ import libraryManApp.libraryConnection;
 public class UserBooksAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	libraryConnection con = new libraryConnection();
-	Connection connection=null;
-	
+	Connection connection = null;
+	private static final Logger logger = LoggerFactory.getLogger(UserBooksAction.class);
 	public void init() throws ServletException{
 		super.init();
 		connection=con.libraryCon();
@@ -48,6 +52,7 @@ public class UserBooksAction extends HttpServlet {
 		Response resSuccess =new Response();
 		resSuccess.setStatusCode(0);
 		PrintWriter writer =response.getWriter();
+		//private static final Logger logger = LoggerFactory.getLogger(UserBooksAction.class);
 		try {
 			/*Class.forName("org.postgresql.Driver");
 			Connection con = DriverManager.getConnection(DB_URL, userName, password);
@@ -60,6 +65,7 @@ public class UserBooksAction extends HttpServlet {
 			
 			ResultSet rs = pst.executeQuery();
 			System.out.println("Query ok");
+			logger.info("query ok");
 			while(rs.next()) {
 				UserBookDetails book = new UserBookDetails();
 				book.setTitle(rs.getString(1));
@@ -69,9 +75,11 @@ public class UserBooksAction extends HttpServlet {
 				bookList.add(book);
 			}
 			System.out.println("Data Geted");
+			logger.info("Data Geted");
 			System.out.println("PrintData");
 			for(int i=0; i<bookList.size(); i++) {
 				System.out.println(bookList.get(i));
+				logger.debug("the data is:" + bookList.get(i));
 			}
 			Collections.sort(bookList);
 			System.out.println("PrintData");
